64d988af66d4bc04b297998d05a7e9db7098f40c
JuliusGithinji <julieusirungu7@gmail.com>
2021-12-04 15:33:03 +0300

    Sign in and log in

---
+++ public/js/prof.js
@@ -0,0 +1,8 @@
+function dropToggle(){
+    const toggleDrop = document.querySelector('.drop');
+    toggleDrop.classList.toggle('active')
+};
+function dropTooggle(){
+    const toggleDrop = document.querySelector('.drop');
+    toggleDrop.classList.toggle('passive')
+}
